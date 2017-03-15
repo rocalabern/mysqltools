7703b6c084fac376d402b33f98a89e744e362e18
Roc Alabern <roc.alabern@gmail.com>
2017-03-15 10:10:11 +0100

    Added snippet for crud operations.

---
+++ inst/examples/mysqltools_crud.R
@@ -0,0 +1,31 @@
+digorig::do.init()
+ch = ms.connect(host=connData$db_mysql_da_ip)
+
+ms.showSchemas(ch)
+ms.showTables(ch, "risk")
+
+ms.printSchemas(ch)
+ms.printTables(ch, "identity")
+ms.listTables(ch, "identity")
+
+ms.Table.Exists(ch, "risk.alerts_blocked_users")
+ms.Table.Exists(ch, "risk.asdasd")
+
+df_r = data.frame(col_A=letters[1:10], col_B=1:10)
+ms.Table.Drop(ch, "risk.prova_r")
+ms.Table.Create(ch, "risk.prova_r", df_r)
+df_db = ms.Table.Read(ch, "risk.prova_r")
+ms.Table.Truncate(ch, "risk.prova_r")
+df_db_empty = ms.Table.Read(ch, "risk.prova_r")
+ms.Table.Drop(ch, "risk.prova_r")
+ms.Table.Drop(ch, "risk.prova_r")
+df_r
+df_db
+df_db_empty
+
+ms.Table.Create(ch, "risk.prova_r", df_r)
+ms.colnames(ch, "risk.prova_r")
+ms.Table.Info(ch, "risk.prova_r")
+ms.Table.Drop(ch, "risk.prova_r")
+
+ms.close(ch)
